cdd720c58263aeb5fa351b7be217765775ad4d07
balibou <benjamin.cherion@gmail.com>
2016-04-16 16:35:02 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularCsvToChartApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

fb722b1b7198c7ee99477efc7838ce149271a302
balibou <benjamin.cherion@gmail.com>
2016-04-16 17:09:34 +0200

    Provide a way for the user to upload a CSV formatted data file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name angularCsvToChartApp
- * @description
- * # angularCsvToChartApp
- *
- * Main module of the application.
- */
 angular
   .module('angularCsvToChartApp', [
     'ngAnimate',
@@ -15,7 +7,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngCsvImport'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -23,11 +16,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,37 @@
 'use strict';
 
 angular.module('angularCsvToChartApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ['$scope', '$parse', function ($scope, $parse) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    $scope.csv = {
+    	content: null,
+    	header: true,
+    	headerVisible: true,
+    	separator: ',',
+    	separatorVisible: true,
+    	result: null,
+    	encoding: 'ISO-8859-1',
+    	encodingVisible: true,
+    };
+
+    var _lastGoodResult = '';
+    $scope.toPrettyJSON = function (json, tabWidth) {
+			var objStr = JSON.stringify(json);
+			var obj = null;
+			try {
+				obj = $parse(objStr)({});
+			} catch(e){
+				// eat $parse error
+				return _lastGoodResult;
+			}
+
+			var result = JSON.stringify(obj, null, Number(tabWidth));
+			_lastGoodResult = result;
+
+			return result;
+    };
+  }]);
